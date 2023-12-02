fdcf92f40e4329fe80f015210f98e8038384d5e7
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-12-02 16:00:35 +0000

    use pointers instead

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -1,38 +1,34 @@
 package sort
 
-func sort(arr []int, low, high int) []int {
+func QuickSort(arr *[]int) {
+	sort(arr, 0, len(*arr)-1)
+}
+
+func sort(arr *[]int, low, high int) {
 	if low < high {
-		var p int
-		arr, p = partition(arr, low, high)
-		arr = sort(arr, low, p-1)
-		arr = sort(arr, p+1, high)
+		p := partition(arr, low, high)
+		sort(arr, low, p-1)
+		sort(arr, p+1, high)
 	}
-	return arr
 }
 
-func QuickSort(arr []int) []int {
-	return sort(arr, 0, len(arr)-1)
-}
+func partition(arr *[]int, low, high int) int {
+	p := (*arr)[low]
+	i := low
+	j := high
 
-func partition(arr []int, low, high int) ([]int, int) {
-	p := arr[low]
-	i := low  // Search for elemets bigger than pivot
-	j := high // Search for elements smaller than pivot
 	for i < j {
-		// Increment i until you find a bigger element
-		for arr[i] <= p {
+		for (*arr)[i] <= p {
 			i++
 		}
-		// Increment j until you find a smaller element
-		for arr[j] > p {
+		for (*arr)[j] > p {
 			j--
 		}
-		// Swap them
 		if i < j {
-			arr[i], arr[j] = arr[j], arr[i]
+			(*arr)[i], (*arr)[j] = (*arr)[j], (*arr)[i]
 		}
 	}
-	arr[low], arr[j] = arr[j], arr[low]
-	// j is the new position of pivot
-	return arr, j
+
+	(*arr)[low], (*arr)[j] = (*arr)[j], (*arr)[low]
+	return j
 }
--- sort/quick_sort_test.go
+++ sort/quick_sort_test.go
@@ -8,10 +8,10 @@ import (
 )
 
 func TestQuickSort(t *testing.T) {
-	a := []int{2, 3, 1, 5}
+	a := &[]int{2, 3, 1, 5}
 	expected := []int{1, 2, 3, 5}
-	result := sort.QuickSort(a)
-	if !reflect.DeepEqual(result, expected) {
-		t.Errorf("For inputs %d, expected %v, but got %v", a, expected, result)
+	sort.QuickSort(a)
+	if !reflect.DeepEqual(*a, expected) {
+		t.Errorf("For inputs %d, expected %v, but got %v", a, expected, a)
 	}
 }
