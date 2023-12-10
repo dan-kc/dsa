5051db026bfab046ec810042202df543bee50018
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-12-10 16:50:06 +0000

    back to 'main'

--- cses/introductory/repetitions.go
+++ cses/introductory/repetitions.go
@@ -1,4 +1,12 @@
-func Repetitions(sequence string) int {
+package main
+
+import "fmt"
+
+// Time: O(n)
+// Space: O(1)
+func main() {
+	var sequence string
+	fmt.Scan(&sequence)
 	maxRepetition := 1
 	currentRepetition := 1
 	for i := 1; i < len(sequence); i++ {
@@ -14,6 +22,26 @@ func Repetitions(sequence string) int {
 	if currentRepetition > maxRepetition {
 		maxRepetition = currentRepetition
 	}
+	fmt.Println(maxRepetition)
+}
 
-	return maxRepetition
+// Time: O(n)
+// Space: O(n)
+func main2() {
+	var input string
+	fmt.Scan(&input)
+
+	repMap := make(map[rune]int)
+	for _, r := range input {
+		repMap[r]++
+	}
+
+	largest := 0
+	for _, v := range repMap {
+		if v > largest {
+			largest = v
+		}
+	}
+
+	fmt.Println(largest)
 }
