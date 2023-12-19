3e1e7dcc47fa510a822f4e4b78d3495794965556
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-12-19 21:31:35 +0000

    refaccy

--- search/binary_search.go
+++ search/binary_search.go
@@ -1,19 +1,27 @@
-package search
+package main
 
-func BinarySearch(needle int, haystack []int) int {
-	// l inclusive, h exclusive
+import "fmt"
+
+func main() {
+	test := []int{2, 8, 13, 22, 38, 45, 53, 87, 99}
+	fmt.Print(binarySearch(test, 45))
+}
+
+// a  binary search only operates on an ordered array
+func binarySearch(input []int, target int) int {
+	// l inclusive, h exclusive (consider target at end)
 	l := 0
-	h := len(haystack)
-	m := l + (h-l)/2
+	h := len(input)
 	for l < h {
-		if needle == haystack[m] {
+		m := l + (h-l)/2
+		if target == input[m] {
 			return m
-		} else if needle > haystack[m] {
-			l = m + 1
-			m = l + (h-l)/2
-		} else {
+		}
+		if target < input[m] {
 			h = m - 1
-			m = l + (h-l)/2
+		}
+		if target > m {
+			l = m + 1
 		}
 	}
 	return -1
