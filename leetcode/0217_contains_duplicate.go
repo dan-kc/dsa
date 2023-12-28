b142edd0152c19b79c6e8446b5704ab199727b7f
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-12-28 15:22:54 +0000

    update

--- leetcode/0217_contains_duplicate.go
+++ leetcode/0217_contains_duplicate.go
@@ -1,9 +1,7 @@
 package leetcode
 
 import (
-	"reflect"
 	"slices"
-	"testing"
 )
 
 // Time: O(nlog(n))
@@ -13,19 +11,6 @@ func ContainsDuplicate(nums []int) bool {
 	for i := 1; i < len(nums); i++ {
 		if nums[i] == nums[i-1] {
 			return true
-		}
-	}
-	return false
-}
-
-// Time: O(n^2)
-// Space: O(1)
-func ContainsDuplicateAlt(nums []int) bool {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] == nums[j] {
-				return true
-			}
 		}
 	}
 	return false
@@ -42,21 +27,4 @@ func ContainsDuplicateAlt2(nums []int) bool {
 		seen[i] = true
 	}
 	return false
-}
-
-func TestContainsDuplicate(t *testing.T) {
-	testCases := []struct {
-		input    []int
-		expected bool
-	}{
-		{[]int{1, 2, 3, 1}, true},
-		{[]int{1, 2, 3, 4}, false},
-		{[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true},
-	}
-	for _, v := range testCases {
-		result := leetcode.ContainsDuplicate(v.input)
-		if !reflect.DeepEqual(result, v.expected) {
-			t.Errorf("For inputs %d,  expected %v, but got %v", v.input, v.expected, result)
-		}
-	}
 }
