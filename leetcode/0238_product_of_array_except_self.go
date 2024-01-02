4f2f97c18f1d79bc95b7a41521c87a3d7541d35a
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2024-01-02 12:14:20 +0000

    refaccy

--- leetcode/0238_product_of_array_except_self.go
+++ leetcode/0238_product_of_array_except_self.go
@@ -1,55 +1,33 @@
-package leetcode
+package main
 
-import (
-	"reflect"
-	"testing"
-)
+import "fmt"
 
-func ProductExceptSelf(nums []int) (res []int) {
-	var pre, post []int
-
-	for i, v := range nums {
-		if i == 0 {
-			pre = append(pre, i)
-			continue
-		}
-		pre = append(pre, v*pre[i-1])
-	}
-
-	l := len(nums)
-	for i := l - 1; i >= 0; i-- {
-		if i == l-1 {
-			post = append([]int{nums[l-1]}, post...)
-			continue
-		}
-		post = append([]int{nums[l] * pre[i+1]}, post...)
-	}
-	// [1,2,4,5]
-	// pre [1,2,8,40]
-	// post [40,40,20,5]
-
-	pre = append([]int{1}, pre...)
-	post = append(post, 1)
-
-	for i := 0; i < l; i++ {
-		res = append(res, pre[i-1]*post[i+1])
-	}
-	return
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+func main() {
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(solve(nums))
 }
 
-func TestProductExceptSelf(t *testing.T) {
-	testCases := []struct {
-		input    []int
-		expected []int
-	}{
-		{[]int{1, 2, 3, 1}, []int{6, 3, 2, 6}},
-		{[]int{1, 2, 3, 4}, []int{24, 12, 8, 6}},
-		{[]int{-1, 1, 0, -3, 3}, []int{0, 0, 9, 0, 0}},
+func solve(nums []int) []int {
+	// Pre = [1,1,2,6]
+	pre := make([]int, 4)
+	pre[0] = 1
+	for i := 1; i < len(nums); i++ {
+		pre[i] = nums[i-1] * pre[i-1]
 	}
-	for _, v := range testCases {
-		res := leetcode.ProductExceptSelf(v.input)
-		if !reflect.DeepEqual(res, v.expected) {
-			t.Errorf("Input: %v, Expected: %v, Result: %v", v.input, v.expected, res)
-		}
+
+	// Post = [24,12,4,1]
+	post := make([]int, 4)
+	post[len(post)-1] = 1
+	for i := len(nums) - 2; i >= 0; i-- {
+		post[i] = nums[i+1] * post[i+1]
 	}
+
+	res := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		res[i] = pre[i] * post[i]
+	}
+
+	return res
 }
--- leetcode/0374_top_k_frequent_elements.go
+++ leetcode/0374_top_k_frequent_elements.go
@@ -2,17 +2,17 @@ package leetcode
 
 func TopKFrequent(nums []int, k int) (res []int) {
 	countMap := map[int]int{}
-	countSlice := make([][]int, len(nums)+1)
+	countSlice := make([][]int, len(nums))
 
 	for _, num := range nums {
 		countMap[num]++
 	}
 
-	for num, count := range countMap {
-		countSlice[count] = append(countSlice[count], num)
+	for key, val := range countMap {
+		countSlice[val] = append(countSlice[val], key)
 	}
 
-	for i := len(countSlice) - 1; i > 0; i-- {
+	for i := len(countSlice) - 1; i >= 0; i-- {
 		res = append(res, countSlice[i]...)
 		if len(res) == k {
 			return
